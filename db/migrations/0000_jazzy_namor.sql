ccc2e0ddb4d52e0828abea2e381109bf58f0f58c
Eko450-eng <ekrem@wipdesign.de>
2023-05-30 10:57:59 +0200

    Changed schema for database

--- db/migrations/0000_jazzy_namor.sql
+++ db/migrations/0000_jazzy_namor.sql
@@ -71,3 +71,9 @@ DO $$ BEGIN
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "question_votes" ADD CONSTRAINT "question_votes_option_option_id_fk" FOREIGN KEY ("option") REFERENCES "option"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
